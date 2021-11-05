267676155b3b412c24957d53186cba5482a97b87
joseph-azurin <77916650+joseph-azurin@users.noreply.github.com>
2021-11-05 06:13:47 -0400

    Python
    Boolean Comparisons

---
+++ jaa-assignment2-part1.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#1
+True
+False
+x=6
+y=3
+print((x > y) and (y < x)) 
+
+
+# In[8]:
+
+
+#2 
+print((1==1) and (2==1))  
+
+
+# In[9]:
+
+
+#3
+print(("pizza") == ("pizza")) 
+
+
+# In[10]:
+
+
+#4
+print(("dog") == ("doggie"))  
+
+
+# In[19]:
+
+
+#5
+print(not(y > x) and (y < x))
+
+
+# In[21]:
+
+
+#6
+print(not(3==4) or (2!=9))
+
+
+# In[27]:
+
+
+#7
+print(("peanut" == ("butter")) and not (3 == 4))
+
+
+# In[28]:
+
+
+#8
+print(not(1==1) or (2==2))
+
+
+# In[31]:
+
+
+#9
+print ((123) == ("123)"))
+
+
+# In[33]:
+
+
+#10
+
+print (("cyber") != ("cyber)"))
+
+
+# In[ ]:
+
+
+
+
